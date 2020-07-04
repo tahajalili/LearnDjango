75d6a5a1772ea59ed36447058e8f9236094ed17e
Taha Jalili <tahajalili@gmail.com>
2020-07-04 17:17:35 +0400

    Fixed a security bug

--- task2/mb_project/mb_project/settings.py
+++ task2/mb_project/mb_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8g##a9qyfd450i0a4pbjfnciqm4yfk467t)axhwro66z@l7*ie'
+SECRET_KEY = "SECRET KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
